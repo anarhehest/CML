afe7e222c9022d881e23dbf613fb33d397ed8146
anarhehest <drevv5@icloud.com>
2025-07-31 21:11:32 +0300

    cml: load minor fix

--- lib/cml.py
+++ lib/cml.py
@@ -9,45 +9,41 @@ class CML:
         return [x.strip() for x in content.split('\n')]
 
     @staticmethod
-    def load(stream: TextIOWrapper):
+    def load(stream: TextIOWrapper) -> Mapping:
 
-        def parse(tokens: Iterable) -> Mapping:
+        root = {}
+        stack = []
 
-            root = {}
-            stack = []
+        cur_map = root
 
-            cur_map = root
+        for token in CML.__tokenize(stream.read()):
 
-            for token in tokens:
+            if token.startswith('<'):
+                key = token[1:].strip()
+                new_map = {}
+                cur_map[key] = new_map
+                stack.append(new_map)
+                cur_map = new_map
 
-                if token.startswith('<'):
-                    key = token[1:].strip()
-                    new_map = {}
-                    cur_map[key] = new_map
-                    stack.append(new_map)
-                    cur_map = new_map
+            elif token == '>':
+                if len(stack) > 1:
+                    stack.pop()
+                    cur_map = stack[-1]
 
-                elif token == '>':
-                    if len(stack) > 1:
-                        stack.pop()
-                        cur_map = stack[-1]
+            elif token.startswith('+'):
+                if 'pros' not in cur_map:
+                    cur_map['pros'] = []
+                cur_map['pros'].append(token[1:].strip())
 
-                elif token.startswith('+'):
-                    if 'pros' not in cur_map:
-                        cur_map['pros'] = []
-                    cur_map['pros'].append(token[1:].strip())
+            elif token.startswith('-'):
+                if 'cons' not in cur_map:
+                    cur_map['cons'] = []
+                cur_map['cons'].append(token[1:].strip())
 
-                elif token.startswith('-'):
-                    if 'cons' not in cur_map:
-                        cur_map['cons'] = []
-                    cur_map['cons'].append(token[1:].strip())
+            elif not token:
+                continue
 
-                elif not token:
-                    continue
+            else:
+                raise ValueError
 
-                else:
-                    raise ValueError
-
-            return root
-
-        return parse(CML.__tokenize(stream.read()))
+        return root
