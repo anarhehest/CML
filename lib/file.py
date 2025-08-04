cbe174a8b84a7e4411260ab16351c1d185b9f342
anarhehest <drevv5@icloud.com>
2025-08-04 20:44:00 +0300

    minor code fixes

--- lib/file.py
+++ lib/file.py
@@ -7,15 +7,15 @@ class File:
 
     @staticmethod
     def load(file_path):
-        match os.path.splitext(file_path)[-1]:
+        _, ext = os.path.splitext(file_path)
+        match ext.lower():
+            case ".cml":
+                func = CML.load
             case ".json":
                 func = json.load
             case ".yml" | ".yaml":
                 func = yaml.safe_load
-            case ".cml":
-                func = CML.load
             case _:
                 raise NotImplementedError
         with open(file_path, 'r') as stream:
             return func(stream)
-
